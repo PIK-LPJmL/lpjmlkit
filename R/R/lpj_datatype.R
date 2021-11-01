a90d742450859b3657c2c4d4e71749560dc9d260
Sebastian Ostberg <ostberg@pik-potsdam.de>
2021-11-01 10:08:12 +0000

    Update lpjDatatype.R to all snake_case, including file name

--- R/R/lpj_datatype.R
+++ R/R/lpj_datatype.R
@@ -3,31 +3,31 @@
 #' @description Provide information on the data type used in an LPJmL file
 #'     based on the 'datatype' attribute included in the file header.
 #' 
-#' @param header Header list object as returned by readHeader() or newHeader()
+#' @param header Header list object as returned by read_header() or new_header()
 #'
 #' @return The function returns a list object with three components:
 #' * type: R data type; can be used with 'what' parameter of readBin()
 #' * size: size of data type; can be used with 'size' parameter of readBin()
-#' * signed: whether or not the data type is signed, only relevant for integers with size 1 or 2; can be used with 'signed' parameter of readBin()
+#' * signed: whether or not the data type is signed, only really relevant for integers with size 1 or 2; can be used with 'signed' parameter of readBin()
 #'
 #' @examples
 #' \dontrun{
 #' # read file header
-#' header <- readHeader("filename.clm")
+#' header <- read_header("filename.clm")
 #' # open file for reading
 #' fp <- file("filename.clm", "rb")
 #' # skip over file header
-#' seek(fp, headerSize(header))
+#' seek(fp, header_size(header))
 #' # read in file data
-#' fileData <- readBin(fp, what=lpjDatatype(header)$type, size=lpjDatatype(header)$size, signed=lpjDatatype(header)$signed, n=header[["header"]]["ncell"]*header[["header"]]["nbands"]*header[["header"]]["nyear"], endian=header[["endian"]])
+#' file_data <- readBin(fp, what=lpj_datatype(header)$type, size=lpj_datatype(header)$size, signed=lpj_datatype(header)$signed, n=header[["header"]]["ncell"]*header[["header"]]["nbands"]*header[["header"]]["nyear"], endian=header[["endian"]])
 #' # close file
 #' close(fp)
 #' }
 #'
-#' @seealso [readHeader()] for reading headers from LPJmL files, [newHeader()] for creating headers from scratch
+#' @seealso [read_header()] for reading headers from LPJmL files, [new_header()] for creating headers from scratch, [header_size()] for determining the size of file headers
 #'
 #' @export
-lpjDatatype <- function(header) {
+lpj_datatype <- function(header) {
   if(is.list(header) && !is.null(header$header)) {
     header <- header$header
   } 
