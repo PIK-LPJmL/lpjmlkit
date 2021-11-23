06755be627f35917edf8fd8ba4b40d5fa2252b10
Jannes <jannesbr@pik-potsdam.de>
2021-11-23 14:28:20 +0100

    add functionality to use output ids based on available outputs of outputvars.js

--- tests/testthat/test-write_config.R
+++ tests/testthat/test-write_config.R
@@ -42,3 +42,45 @@ test_that("write correct config", {
       check_tibble[1, which(tmp_objects != "dependency")]))
 
 })
+
+
+test_that("include non output defined outputvars", {
+
+    test_params <- tibble::tibble(
+      sim_name = c("transient_pnv"),
+      order = c(2),
+      dependency = c("testdep"),
+      )
+
+  # create template that would usually be created by writeConfig directly
+  test_tmp <- tibble::tibble(sim_name = NA,
+                             config_file = NA,
+                             order = NA,
+                             dependency = NA)
+
+  # test main function of writeConfig since writeConfig is hard to test
+  tmp_objects <- write_single_config(params = test_params,
+                                     model_path = "testthat",
+                                     output_path = "testthat",
+                                     output_list = c("grid", "irrig"),
+                                     output_format = "clm",
+                                     js_filename = "lpjml.js",
+                                     config_tmp = test_tmp,
+                                     test_it = TRUE)
+  print(tmp_objects)
+  # check if defined outputvar (id) exists as last output
+  expect_true(
+    tmp_objects[[1]][["output"]][[
+      length(tmp_objects[[1]][["output"]])
+    ]]$id == "irrig"
+  )
+
+  # check if filename is set correctly
+  expect_true(
+    grepl("mirrig.clm",
+          tmp_objects[[1]][["output"]][[
+            length(tmp_objects[[1]][["output"]])
+          ]]$file$name)
+  )
+
+})
