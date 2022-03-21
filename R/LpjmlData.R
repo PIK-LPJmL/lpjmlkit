59a24d1dd805bf2da3e3b03125a50bcc12a54663
Jannes <jannesbr@pik-potsdam.de>
2022-03-21 22:03:53 +0100

    more methods, private fields, better self print

--- R/LpjmlData.R
+++ R/LpjmlData.R
@@ -15,74 +15,98 @@
 # https://adv-r.hadley.nz/r6.html#r6-classes, also why CamelCase is used ...
 LpjmlData <- R6::R6Class(
   classname = "LpjmlData",
-  lock_objects = TRUE,
+  lock_objects = FALSE,
   public = list(
     meta_data = NULL,
     data = NULL,
     # init function
     initialize = function(data_array, meta_data = NULL) {
-      if (is(meta_data, "LpjmlMetaData")) {
+      if (is(meta_data, "LpjmlMetaData") | is(meta_data, "NULL")) {
         self$meta_data <- meta_data
-      } else if (is(meta_data, "NULL")) {
-        self$meta_data <- NULL
       } else {
         stop("Provide a LpjmlMetaData object for meta_data.")
       }
       self$data <- data_array
     },
-    `[` = function(...) {
-      self$data[...]
+    array = function(subset_list = NULL) {
+      self$data %>% {
+          ifelse(is.null(subset_list), ., asub(., subset_list))
+        } %>%
+      return()
+    },
+    tibble = function(subset_list = NULL, value_name = "value") {
+      self$meta_data$.__update_subset__()
+      self$data %>% {
+          ifelse(is.null(subset_list), ., asub(., subset_list))
+        } %>%
+        reshape2::melt(value.name = value_name) %>%
+        as_tibble() %>%
+        return()
+    },
+    raster = function(grid_file, as_layers = "bands", subset_list = NULL) {
+
+    },
+    brick = function(grid_file, as_layers = "bands", subset_list = NULL) {
+
+    },
+    rast = function(grid_file, as_layers = "bands", subset_list = NULL) {
+
     },
     length = function() {
-      length(self$data)
+      return(length(self$data))
     },
     dim = function() {
       dim(self$data)
     },
+    dimnames = function() {
+      dimnames(self$data)
+    },
+    summary = function(dimension="bands") {
+      if (dimension %in% names(dimnames(self$data))) {
+        self$data %>%
+          apply(dimension, c) %>%
+          summary()
+      } else {
+        mat_sum <- summary(matrix(self$data))
+        space_len <- ifelse(nchar(self$meta_data$variable) > 8,
+                            0,
+                            4 - sqrt(nchar(self$meta_data$variable)))
+        paste0(c(rep(" ", space_len), self$meta_data$variable, "\n")) %>%
+          append(paste0(mat_sum, "\n")) %>%
+          cat()
+      }
+    },
     print = function() {
       blue_col <- "\u001b[34m"
       unset_col <- "\u001b[0m"
-      meta_fields <- unlist(
-        as.list(self$meta_data)[self$meta_data$.__meta_fields_set__]
-      )
-      if (!is.null(self$meta_data$.__meta_fields_set__)) {
-        cat(paste0(blue_col, "$meta_data", unset_col, "\n"))
-        cat(
-          paste0("  ",
-                 blue_col,
-                 "$",
-                 names(meta_fields),
-                 unset_col,
-                 " ",
-                 meta_fields,
-                 collapse = "\n")
-        )
-        cat("\n")
-      }
-      data_dims <- lapply(
-        dimnames(self$data),
-        function(x) {
-          if (length(x) > 30) {
-            return(append(x[1:4], c("...", tail(x, n = 1))))
+      cat(paste0(blue_col, "$meta_data", unset_col, "\n"))
+      self$meta_data$print(spaces = "  ")
+      if (is.null(self$meta_data$subset)) {
+        dim_names <- self$dimnames()
+        cat(paste0(blue_col, "$data$dimnames()", unset_col, "\n"))
+        for (sub in seq_along(dim_names)) {
+          to_char2 <- ifelse(is.character(dim_names[[sub]]), "\"", "")
+          if (length(dim_names[[sub]]) > 30) {
+            abbr_dim_names <- paste0(c(paste0(to_char2,
+                                              dim_names[[sub]][1:4],
+                                              to_char2),
+                                    "...",
+                                    paste0(to_char2,
+                                           tail(dim_names[[sub]], n = 1),
+                                           to_char2)))
           } else {
-            return(x)
+            abbr_dim_names <- paste0(to_char2, dim_names[[sub]], to_char2)
           }
+          cat("",
+              blue_col,
+              paste0("$", names(dim_names[sub])),
+              unset_col,
+              abbr_dim_names)
+          cat("\n")
         }
-      )
-      cat(paste0(blue_col, "$data", unset_col, "\n"))
-      cat(paste0("  ", blue_col, "%>% head", unset_col, "\n"))
-      cat("   ", head(self$data), "\n")
-      cat(paste0("  ", blue_col, "%>% dim", unset_col, "\n"))
-      cat(
-        paste0("    ",
-               names(dim(self$data)),
-               ": ",
-               dim(self$data),
-               collapse = "\n")
-      )
-      cat("\n")
-      cat(paste0("  ", blue_col, "%>% dimnames", unset_col, "\n"))
-      cat(paste0("    ", names(data_dims), ": ", data_dims, collapse = "\n"))
+      }
+      cat(paste0(blue_col, "$data$summary()", unset_col, "\n"))
+      print(self$summary())
       cat("\n")
     }
   )
@@ -91,10 +115,11 @@ LpjmlData <- R6::R6Class(
 # set up method dispatch
 #   https://stackoverflow.com/questions/50842251/define-a-bracket-operator-on-an-r6-class
 `[.LpjmlData`    <- function(obj, ...) obj$`[`(...)
-# `[<-.LpjmlData`  <- function(obj, ...) obj$`[<-`(...)
 length.LpjmlData <- function(obj, ...) obj$length(...)
-
+dim.LpjmlData <- function(obj, ...) obj$dim(...)
+dimnames.LpjmlData <- function(obj, ...) obj$dimnames(...)
+summary.LpjmlData <- function(obj, ...) obj$summary(...)
 
 # meta_data = read_meta("/p/projects/open/Jannes/lpjml/testing/meta/runs/output/lu/aconv_loss_evap.bin.json")
-# data_array <- array(1, dim=c(cells=67420,months=12,bands=12), dimnames=list(cells=1:67420,months=1:12,years=2001:2012))
+# data_array <- array(1, dim=c(cells=67420,months=12,bands=10), dimnames=list(cells=1:67420,months=1:12, bands=2011:2020))
 # oo = LpjmlData$new(data_array, meta_data)
--- R/LpjmlMetaData.R
+++ R/LpjmlMetaData.R
@@ -14,50 +14,32 @@
 LpjmlMetaData <- R6::R6Class(
   classname = "LpjmlMetaData",
   lock_objects = FALSE,
-  private = list(
-    .__meta_fields_set__ = NULL
-  ),
   public = list(
-    sim_name = NULL,
-    source = NULL,
-    history = NULL,
-    variable = NULL,
-    firstcell = NULL,
-    ncell = NULL,
-    cellsize_lon = NULL,
-    cellsize_lat = NULL,
-    nstep = NULL,
-    timestep = NULL,
-    nbands = NULL,
-    band_names = NULL,
-    descr = NULL,
-    unit = NULL,
-    firstyear = NULL,
-    lastyear = NULL,
-    nyear = NULL,
-    datatype = NULL,
-    scalar = NULL,
-    order = NULL,
-    bigendian = FALSE,
-    format = NULL,
-    filename = NULL,
-
     # init function
     initialize = function(meta_list) {
       for (idx in seq_along(meta_list)) {
-        if (!names(meta_list[idx]) %in% names(LpjmlMetaData$public_fields)) {
-          warning(paste0(names(meta_list[idx]),
-                         " may not be a valid LpjmlMetaData field."))
-        }
-        do.call("$<-", list(self,
-                            names(meta_list[idx]),
+        # if (!names(meta_list[idx]) %in% names(LpjmlMetaData$public_fields)) {
+        #   warning(paste0(names(meta_list[idx]),
+        #                  " may not be a valid LpjmlMetaData field."))
+        # }
+        do.call("$<-", list(private,
+                            paste0(".", names(meta_list[idx])),
                             meta_list[[idx]]))
       }
-      self$.__meta_fields_set__ <- names(meta_list)
+      private$.fields_set <- names(meta_list)
     },
-
+    # update supplied subset_list in self.subset
+    update_subset = function(subset_list) {
+      # evtly TODO: if subset_list integer -> index of previus subset_list
+      # TODO: convert from time to years/months/days and vice versa
+      private$.subset[
+        names(subset_list) %in% names(self$dimension_map)
+      ] <- subset_list[
+        names(subset_list) %in% names(self$dimension_map)
+      ]
+    },
     # convert to header object
-    header = function() {
+    as_header = function() {
       create_header(
         name = "LPJ_OUT",
         version = 4,
@@ -76,28 +58,178 @@ LpjmlMetaData <- R6::R6Class(
         verbose = TRUE
       )
     },
-    fields = function() {
-      names(LpjmlMetaData$public_fields)
-    },
     # return fields set as list
-    list = function() {
-      all_list <- as.list(self) %>%
-        `[`(self$.__meta_fields_set__)
-      return(all_list)
+    as_list = function() {
+      self$fields_set %>%
+        sapply(function(x) do.call("$", list(self, x)), simplify = FALSE) %>%
+        return()
     },
-    print = function() {
+    print = function(spaces = "") {
       blue_col <- "\u001b[34m"
       unset_col <- "\u001b[0m"
-      meta_fields <- unlist(as.list(self)[self$.__meta_fields_set__])
+      meta_fields <- self$fields_set %>%
+        sapply(function(x) do.call("$", list(self, x)),
+               USE.NAMES = FALSE)
+      to_char1 <- self$fields_set %>%
+        sapply(function(x) {
+          if (is.character(do.call("$", list(self, x)))) {
+            return("\"")
+          } else {
+            return("")
+          }
+        },
+        USE.NAMES = FALSE)
       cat(
-        paste0(blue_col,
-               names(meta_fields),
+        paste0(spaces,
+               blue_col,
+               "$",
+               self$fields_set,
                unset_col,
                " ",
+               to_char1,
                meta_fields,
+               to_char1,
                collapse = "\n")
       )
       cat("\n")
+      if (!is.null(private$.subset)) {
+        cat(paste0(spaces, blue_col, "$subset", unset_col, "\n"))
+        for (sub in seq_along(private$.subset)) {
+          to_char2 <- ifelse(is.character(private$.subset[[sub]]), "\"", "")
+          if (length(private$.subset[[sub]]) > 30) {
+            abbr_subset <- paste0(c(paste0(to_char2,
+                                           private$.subset[[sub]][1:4],
+                                           to_char2),
+                                    "...",
+                                    paste0(to_char2,
+                                           tail(private$.subset[[sub]], n = 1),
+                                           to_char2)))
+          } else {
+            abbr_subset <- paste0(to_char2, private$.subset[[sub]], to_char2)
+          }
+          cat(spaces,
+              blue_col,
+              paste0("$", names(private$.subset[sub])),
+              unset_col,
+              abbr_subset)
+          cat("\n")
+        }
+      }
     }
+  ),
+  active = list(
+    sim_name = function() {
+      return(private$.sim_name)
+    },
+    source = function() {
+      return(private$.source)
+    },
+    history = function() {
+      return(private$.history)
+    },
+    variable = function() {
+      return(private$.variable)
+    },
+    firstcell = function() {
+      return(private$.firstcell)
+    },
+    ncell = function() {
+      return(private$.ncell)
+    },
+    cellsize_lon = function() {
+      return(private$.cellsize_lon)
+    },
+    cellsize_lat = function() {
+      return(private$.cellsize_lat)
+    },
+    nstep = function() {
+      return(private$.nstep)
+    },
+    timestep = function() {
+      return(private$.timestep)
+    },
+    nbands = function() {
+      return(private$.nbands)
+    },
+    band_names = function() {
+      return(private$.band_names)
+    },
+    descr = function() {
+      return(private$.descr)
+    },
+    unit = function() {
+      return(private$.unit)
+    },
+    firstyear = function() {
+      return(private$.firstyear)
+    },
+    lastyear = function() {
+      return(private$.lastyear)
+    },
+    nyear = function() {
+      return(private$.nyear)
+    },
+    datatype = function() {
+      return(private$.datatype)
+    },
+    scalar = function() {
+      return(private$.scalar)
+    },
+    order = function() {
+      return(private$.order)
+    },
+    bigendian = function() {
+      return(private$.bigendian)
+    },
+    format = function() {
+      return(private$.format)
+    },
+    filename = function() {
+      return(private$.filename)
+    },
+    subset = function() {
+      return(private$.subset)
+    },
+    fields_set = function() {
+      return(private$.fields_set)
+    },
+    dimension_map = function() {
+      return(private$.dimension_map)
+    }
+  ),
+  private = list(
+    .sim_name = NULL,
+    .source = NULL,
+    .history = NULL,
+    .variable = NULL,
+    .firstcell = NULL,
+    .ncell = NULL,
+    .cellsize_lon = NULL,
+    .cellsize_lat = NULL,
+    .nstep = NULL,
+    .timestep = NULL,
+    .nbands = NULL,
+    .band_names = NULL,
+    .descr = NULL,
+    .unit = NULL,
+    .firstyear = NULL,
+    .lastyear = NULL,
+    .nyear = NULL,
+    .datatype = NULL,
+    .scalar = NULL,
+    .order = NULL,
+    .bigendian = FALSE,
+    .format = NULL,
+    .filename = NULL,
+    .subset = NULL,
+    .fields_set = NULL,
+    .dimension_map = list(cells = "cells",
+                         time = c("years", "months", "days"),
+                         years = "time",
+                         months = "time",
+                         days = "time",
+                         bands = "bands")
   )
 )
+
+as.list.LpjmlMetaData <- function(obj, ...) obj$as_list(...)
