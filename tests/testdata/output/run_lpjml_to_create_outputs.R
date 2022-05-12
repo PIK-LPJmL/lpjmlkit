29a8a76b4130126d5fd682b521b4709644e98c2e
Sara Minoli <sara.minoli@pik-potsdam.de>
2022-05-12 14:54:47 +0200

    Adding: (1) test data for read_output(); (2) a script to run lpjml with standard lpjml.js (lpjml version sha: b728aed9) config and genarate osample outputs for testing; (3) tests/testthat/test-read_output.R

---
+++ tests/testdata/output/run_lpjml_to_create_outputs.R
@@ -0,0 +1,86 @@
+# ---------------------------------------------------------------------------- #
+# Project:     lpjmlKit
+# ------------------------------------ #
+# Author:      Sara Minoli
+# Email:       sara.minoli@pik-potsdam.de
+# Description: Run LPJmL to create a sample output file for package tests
+# ---------------------------------------------------------------------------- #
+
+rm(list = ls(all = T))
+
+library(lpjmlKit)
+library(tibble)
+library(magrittr)
+
+
+# ------------------------------------ #
+# Paths ----
+lpjdir <- paste0("/home/minoli/LPJmL_GitLab/nitrogen_cotton_tillage_newoutput/")
+outdir <- paste0("/home/minoli/git_lpjmlKit/lpjml_outputs/")
+testdatadir <- paste0("/home/minoli/git_lpjmlKit/lpjmlKit/tests/testdata/")
+
+
+# ------------------------------------ #
+# Create Parameter Tibble ----
+test_param <- tibble(
+    sim_name         = c("test_spinup", "test_transient"),
+    dependency       = c(NA, "test_spinup"),
+    order            = c(1, 2),
+    startgrid        = 10000,
+    endgrid          = 10002,
+    #nspinup          = c(10000, 390),
+    river_routing    = FALSE,
+    `-DDAILY_OUTPUT` = c(FALSE, TRUE),
+    firstyear           = 1901,
+    lastyear            = 2011,
+    outputyear          = 2001,
+    output_metafile  = TRUE
+)
+
+outputs_df <- data.frame(rbind(
+                         cbind(output = "d_lai",   timestep = "daily"),
+                         cbind(output = "transp",  timestep = "monthly"),
+                         cbind(output = "npp",     timestep = "annual"),
+                         cbind(output = "pft_npp", timestep = "annual")
+                         ))
+
+# ------------------------------------ #
+# Write Config Files ----
+config_details <- write_config(
+  params               = test_param,
+  model_path           = lpjdir,
+  js_filename          = "lpjml.js",
+  output_path          = outdir,
+  output_format        = "raw",
+  output_list          = outputs_df$output,
+  output_list_timestep = outputs_df$timestep,
+  debug                = TRUE
+  )
+
+# ------------------------------------ #
+# Run the model
+run_details <- run_lpjml(
+  x           = test_param,
+  model_path  = lpjdir,
+  output_path = outdir
+)
+
+# ------------------------------------ #
+# Copy Files to lpjmlKit test
+
+outputs <- paste0(rep(outputs_df$output, each = 2),
+                 c(".bin", ".bin.json")
+                 )
+
+if (all(file.exists(paste0(outdir, "output/test_transient/", outputs)))) {
+    cat("\nOK! All outputs exist.")
+} else {
+    stop("\nSome outputs are missing!")
+}
+
+file.copy(from = paste0(outdir,      "output/test_transient/", outputs),
+          to   = paste0(testdatadir, "output/", outputs),
+          overwrite = TRUE,
+          copy.mode = TRUE)
+
+cat("\n--- Done! ---")
---
+++ tests/testthat/test-read_output.R
@@ -0,0 +1,31 @@
+test_that("read output - daily", {
+  # test daily output
+  out_file_name <- "../testdata/output/d_lai.bin"
+  out <- read_output(file_name = out_file_name)
+  expect_true(exists("out"))
+  expect_type(out, "LpjmlData")
+})
+
+test_that("read output - monthly", {
+  # test monthy output
+  out_file_name <- "../testdata/output/transp.bin"
+  out <- read_output(file_name = out_file_name)
+  expect_true(exists("out"))
+  expect_type(out, "LpjmlData")
+})
+
+test_that("read output - annual", {
+  # test annual output
+  out_file_name <- "../testdata/output/npp.bin"
+  out <- read_output(file_name = out_file_name)
+  expect_true(exists("out"))
+  expect_type(out, "LpjmlData")
+})
+
+test_that("read output - pft-specific & annual", {
+  # test pft-annual output
+  out_file_name <- "../testdata/output/pft_npp.bin"
+  out <- read_output(file_name = out_file_name)
+  expect_true(exists("out"))
+  expect_type(out1, "LpjmlData")
+})
