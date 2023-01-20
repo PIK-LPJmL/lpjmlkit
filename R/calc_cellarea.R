a212a9c1c4382400f285a829073d3a5feb8b8d1e
Jannes <jannesbr@pik-potsdam.de>
2023-01-20 15:33:54 +0100

    add calc_cellarea support for LPJmLData objects

--- R/calc_cellarea.R
+++ R/calc_cellarea.R
@@ -1,17 +1,21 @@
 #' @title Calculate cell area of LPJmL cells
 #'
-#' @description Calculate cell area of LPJmL cells based on cell
-#'   coordinates and grid resolution. Uses a spherical representation of Earth.
+#' @description Calculate cell area of LPJmL cells based on a LPJmLData object
+#' or cell coordinates and grid resolution.
+#' Uses a spherical representation of Earth.
 #'
-#' @param lat Vector of cell-center latitude coordinates in degrees.
+#' @param x `LPJmLData` object with `$grid` attribute, a LPJmLData object of
+#' variable `"grid"` (`"LPJ_GRID"`) or vector of cell-center latitude
+#' coordinates in degrees.
 #' @param res_lon Grid resolution in longitude direction in degrees
-#'   (default: 0.5).
+#'   (default: 0.5). If x LPJmLData` object it will be ignored if different.
 #' @param res_lat Grid resolution in latitude direction in degrees (default:
-#'   same as res_lon).
-#' @param earth_radius Radius of sphere (in \eqn{m}) used to calculate cell
-#'   areas.
-#'
-#' @return Vector of cell areas in \eqn{m^2} corresponding to cells in `lat`.
+#'   same as res_lon). If x LPJmLData` object it will be ignored if different.
+#' @param unit Character string, defaults to `"m2"`. Further available options
+#' are `"ha"` or `"km2"`
+#' @return Either if x is an LPJmLData object a vector or array matching the
+#' space dimension(s) of this object or if x is a latitude vector a vector of
+#' cell areas in \eqn{m^2} corresponding to cells in `lat` is returned.
 #'
 #' @examples
 #' grid <- matrix(
@@ -23,15 +27,55 @@
 #' gridarea <- calc_cellarea(grid[,"lat"])
 #'
 #' @export
-calc_cellarea <- function(lat,
+calc_cellarea <- function(x,
                           res_lon = 0.5,
                           res_lat = res_lon,
-                          earth_radius = 6371000.785
+                          earth_radius = 6371000.785,
+                          unit = "m2"
                          ) {
+  # worklfow for LPJmLData objects
+  if (methods::is(x, "LPJmLData")) {
+    # check if grid is available as attribute
+    if (!is.null(x$grid)) {
+      x <- x$grid
+    # check if LPJmLData object is of variable grid or LPJ_GRID (header file)
+    } else if (!any(c("grid", "LPJ_GRID") %in% x$meta$variable)) {
+      stop("Grid attribute is missing. Use method add_grid() to add it.")
+    }
+    if (res_lon != x$meta$cellsize_lon) {
+      res_lon <- x$meta$cellsize_lon
+      warning("res_lon is ignored, instead cellsize_lon of x$meta is used.")
+    }
+    if (res_lat != x$meta$cellsize_lat) {
+      res_lat <- x$meta$cellsize_lat
+      warning("res_lat is ignored, instead cellsize_lat of x$meta is used.")
+    }
+    # check for format of space dimensions apply different processing
+    if (x$meta$._space_format_ == "cell") {
+      # for format "cell" latitudes are supplied as data in band dimension
+      #   ("lat")
+      x <- asub(x$data, band = "lat")
+    } else {
+      # for format "lon_lat" latitudes are supplied by dimnames, for calculation
+      #   original array is overwritten with corresponding latitudes
+      x <- check <- x$data
+      asub(x, lat = dimnames(x)$lat) <- as.numeric(dimnames(x)$lat)
+      x[is.na(check)] <- NA
+    }
+  } else {
+    # make sure supplied vector is numeric (lat dimnames of LPJmLData are
+    #   character string)
+    x <- as.double(x)
+  }
   cellwidth <- earth_radius * pi / 180
-  if (any(lat < -90 | lat > 90, na.rm = TRUE)) {
+  if (any(x < -90 | x > 90, na.rm = TRUE)) {
     stop("Invalid latitude values in lat. Values must be within +- 90 degrees")
   }
-  as.double(cellwidth * res_lon) * as.double(cellwidth * res_lat) *
-    as.double(cos(lat / 180 * pi))
+  y <- cellwidth * res_lon * cellwidth * res_lat * cos(x / 180 * pi) %>%
+    # for each unit possiblity apply conversion factor
+    switch(unit,
+           m2 = .,
+           ha = . * 1e-4,
+           km2 = . * 1e-6)
+    return(y)
 }
--- tests/testthat/test-calc_cellarea.R
+++ tests/testthat/test-calc_cellarea.R
@@ -5,3 +5,33 @@ test_that("Calculate cell area", {
   expect_type(calc_cellarea(lats), "double")
   expect_length(calc_cellarea(lats), length(lats))
 })
+
+
+test_that("Calculate cell area with LPJmLData object and grid attribute", {
+  output <- read_io(filename = "../testdata/output/npp.bin.json")
+  # perform adding a grid object
+  output$add_grid()
+  # calculate cell area for each cell
+  cell_area <- calc_cellarea(output, unit = "km2")
+
+  # calculate cell area for lon_lat format
+  output$transform(to = "lon_lat")
+  cell_area2 <- calc_cellarea(output, unit = "km2")
+
+  testthat::expect_equal(as.vector(cell_area),
+                         as.vector(na.omit(cell_area2)))
+})
+
+test_that("Calculate cell area with LPJmLData object of variable grid", {
+  output <- read_io(filename = "../testdata/output/grid.bin.json")
+
+  # calculate cell area for each cell
+  cell_area <- calc_cellarea(output, unit = "km2")
+
+  # calculate cell area for lon_lat format
+  output$transform(to = "lon_lat")
+  cell_area2 <- calc_cellarea(output, unit = "km2")
+
+  testthat::expect_equal(as.vector(cell_area),
+                         as.vector(na.omit(cell_area2)))
+})
