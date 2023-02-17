cce22dd9974ee2fd17d5823f8d8998c7c4ad8857
Jannes <jannesbr@pik-potsdam.de>
2023-02-17 17:26:36 +0100

    support for reading compilable js files such as lpjml.js

--- R/read_config.R
+++ R/read_config.R
@@ -1,11 +1,17 @@
-#' Read an LPJmL config file (JSON)
+#' Read an LPJmL configuration file
 #'
-#' Reads a config JSON file (not `lpjml.js`, but its precompiled version which
-#' can be generated using `parse_config()` or running `cpp -P filename.js` in
-#' the shell) and turns it into a nested list object.
+#' Reads a configuration (config) file (compilable js/csjon file or json file)
+#' and turns it into a nested list object.
 #'
 #' @param filename Character string representing path
 #' (if different from current working directory) and filename.
+#'
+#' @param from_restart Logical, if file is not pre-compiled (no json), defining
+#' whether config files should be read as from_restart (transient run) or
+#' without (spinup run). Defaults to `FALSE` (spinup run)
+#'
+#' @param macro Character string, if file is not pre-compiled (no json), pass
+#' a macro to the pre compiler, e.g. ("-DFROM_RESTART")
 #'
 #' @return A nested list object representing the structure of `config_*.json`.
 #'
@@ -21,9 +27,37 @@
 #'
 #'  config[["input"]][["coord"]][["name"]]
 #'  # [1] "input_VERSION2/grid.bin"
+#'
+#'  # visualize configuration as tree view
+#'  View(config)
 #' }
 #' @export
-read_config <- function(filename) {
+read_config <- function(filename,
+                        from_restart = FALSE,
+                        macro = "") {
+
+  file_ext <- function(x) {
+    pos <- regexpr("\\.([[:alnum:]]+)$", x)
+    ifelse(pos > -1L, substring(x, pos + 1L), "")
+  }
+
+  # Detect file type of config files - compiled json or not pre-compiled
+  # (or not valid)
+  file_type <- file_ext(filename)
+
+  if (file_type == "json") {
     tmp_json <- jsonlite::read_json(path = filename, simplify = FALSE)
+
+  } else if (tolower(file_type) %in% c("cjson", "js")) {
+    tmp_json <- parse_config(
+      path = dirname(filename),
+      js_filename = basename(filename),
+      from_restart = from_restart,
+      macro = macro)
+
+  } else {
+    stop("File type not supported.")
+  }
+
   tmp_json
 }
