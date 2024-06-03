5dc8fa7b3f92807a0ebf2a3bdc3b1812d7d64263
Fabian Stenzel <stenzel@pik-potsdam.de>
2024-06-03 18:41:35 +0200

    continued read_cdf function

--- R/read_cdf.R
+++ R/read_cdf.R
@@ -63,21 +63,26 @@ get_main_variable <- function(file_nc){
 #'
 #' @export
 read_cdf_header <- function( nc_in_file, 
-                          var = NULL,
-                          suppress_read_print = TRUE
+                             var = NULL,
+                             suppress_read_print = TRUE
 ) {
   file_type <- lpjmlkit::detect_io_type(filename = nc_in_file)
   #read_cdf_meta <- lpjmlkit::read_meta(filename = fpc_file_meta)
-  empty_meta <- lpjmlkit::LPJmLMetaData
-  
-  varnames <- names(file_nc$var)
+
   file_nc <- ncdf4::nc_open(filename = nc_in_file)
+  varnames <- names(file_nc$var)
   if (is.null(var)) var <- get_main_variable(file_nc = file_nc)
   
   # get the first of the variable names that has not been identified as the 
   # main variable
-  bands_var_name <- varnames[-match(var,varnames)][1]
-  bands <- ncdf4::ncvar_get(nc = file_nc, varid = bands_var_name)
+  if (length(varnames)>1){
+    bands_var_name <- varnames[-match(var,varnames)][1]
+    bands <- ncdf4::ncvar_get(nc = file_nc, varid = bands_var_name)
+  }else{
+    bands_var_name <- ""
+    bands <- 1
+  }
+
                            
   # get lon/lat information
   dimnames <- names(file_nc$dim)
@@ -87,6 +92,7 @@ read_cdf_header <- function( nc_in_file,
   lat <- file_nc$dim[[latdim]]$vals
   nlonin <- length(lon)
   nlatin <- length(lat)
+  
   # take the median difference between the cells as the resolution
   spatial_difference_lon <- lon[2:length(lon)] - lon[1:(length(lon)-1)]
   resolution_lon <- median(spatial_difference_lon)
@@ -99,21 +105,34 @@ read_cdf_header <- function( nc_in_file,
   timing <- get_timestep(file_nc = file_nc)
   tres <- timing$tres
   firstyr <- timing$firstyr
+  if (tres == "annual"){
+    nyears <- length(tvals)
+    nsteps <- 1
+  }else if (tres == "monthly"){
+    nyears <- length(tvals)/12
+    nsteps <- 12
+  }else {
+    stop("time resolution unknown. (e.g. daily not implemented yet)")
+  }
+  
+  lastyr <- firstyr + nyears - 1
   
   global_attributes <- ncdf4::ncatt_get(file_nc, 0)
   var_attributes <- ncdf4::ncatt_get(file_nc, var)
   
+  empty_meta <- list()
   empty_meta$sim_name <- global_attributes$title
-  empty_meta$source global_attributes$source
-  empty_meta$history global_attributes$history
-  empty_meta$variable <- tolower(var)
+  empty_meta$source <- global_attributes$source
+  empty_meta$history <- global_attributes$history
+  empty_meta$name <- tolower(var)
+  empty_meta$variable <- var
   empty_meta$descr <- var_attributes$longname
   empty_meta$unit <- var_attributes$units
   empty_meta$nbands <- length(bands)
   empty_meta$band_names <- bands
-  empty_meta$nyear <- length(tvals)
+  empty_meta$nyear <- nyears
   empty_meta$firstyear <- firstyr
-  empty_meta$lastyear <- tvals[length(tvals)]
+  empty_meta$lastyear <- lastyr
   empty_meta$ncell <- nlonin*nlatin
   empty_meta$firstcell <- 0
   empty_meta$cellsize_lon <- resolution_lon
@@ -121,36 +140,38 @@ read_cdf_header <- function( nc_in_file,
   empty_meta$scalar <- 1
   empty_meta$format <- file_type
   empty_meta$filename <- basename(nc_in_file)
+  empty_meta$nstep <- nsteps
   
-  empty_meta$nstep <- 1
   empty_meta$timestep <- 1
   empty_meta$datatype <- "float"
   empty_meta$order <- "cellseq"
   empty_meta$bigendian <- FALSE
   empty_meta$subset <- FALSE
+  ncdf4::nc_close(file_nc)
   
-  return(empty_meta)
+  header_data <- lpjmlkit::LPJmLMetaData$new(x = empty_meta)
+  return(header_data)
 }
 
 #' Reads netcdf and returns it as array
 #'
 #' Reads an arbitrary netcdf and returns the values at the lon/lat locations..
 #' Todos:
-#' - deal with input ncs in different resolutions 
-#' - band name variables -> extract them
 #' - annual data which is written out only every X years (what about monthly?):
 #'   (/p/tmp/sibylls/Methane/output_holocene/soilc.nc)
-#' - daily 
+#' - daily timesteps
 #' - check calendar
 #' - check lpj code for nc output creation
+#' - how to distribute nlat and nlon from nc header to read function? 
+#'   or read in again?
 #'
 #' @param nc_in_file netcdf file name
+#' @param nc_header header data, read in from either meta file or data in 
+#'        netcdf file
 #' @param get_year_start first year to be read 
 #'        (if not specified will default to first record year)
 #' @param get_year_stop final year to be read 
 #'        (if not specified will default to last record year)
-#' @param var optional variable to be read, in case automatic detection does 
-#'        not work as intended or several variables are stored within the file
 #' @param suppress_read_print whether to suppress the info "reading file XYZ" 
 #'        (default TRUE)
 #'
@@ -162,12 +183,12 @@ read_cdf_header <- function( nc_in_file,
 #'
 #' @export
 read_cdf <- function( nc_in_file, 
-                             var = NULL,
-                             get_year_start = NULL,
-                             get_year_stop = NULL,
-                             suppress_read_print = TRUE
+                      nc_header,
+                      get_year_start = NULL,
+                      get_year_stop = NULL,
+                      suppress_read_print = TRUE
 ) {
-
+  var <- nc_header$variable
   file_nc <- ncdf4::nc_open(filename = nc_in_file)
   if (!suppress_read_print) {
     print(paste0("Reading in: ", nc_in_file))
@@ -175,47 +196,32 @@ read_cdf <- function( nc_in_file,
                  ". If this is not correct,",
                  " please specify manually via argument var."))
   }
-  if (ndims == 3){
-    data <- ncdf4::ncvar_get(nc = file_nc, varid = var,
-                             start = c(1,1,1), count = c(1,1,-1))
-    data_dim <- dim(data)
-    nbands <- 1
-    tsteps <- data_dim[1]
-  }else if (ndims >= 4){
-    data <- ncdf4::ncvar_get(nc = file_nc, varid = var,
-                             start = c(1,1,1,1), count = c(1,1,-1,-1))
-    data_dim <- dim(data)
-    nbands <- data_dim[1]
-    tsteps <- data_dim[2]
-  }else{
-    stop("Less than 3 dimensions for file. aborting.")
-  }
-  if (tres == "annual"){
-    nyears <- tsteps
-    nmonths <- 1
-  }else if (tres == "monthly"){
-    nyears <- tsteps/12
-    nmonths <- 12
-  }
-  if (is.null(get_year_start)) get_year_start <- firstyr
-  if (is.null(get_year_stop)) get_year_stop <- firstyr + nyears - 1
+
+  if (is.null(get_year_start)) get_year_start <- nc_header$firstyear
+  if (is.null(get_year_stop)) get_year_stop <- nc_header$lastyear
   ngetyears <- get_year_stop - get_year_start + 1
-
-
-  outdata <- array(0, dim = c(nlonin,nlatin,nbands,nmonths,ngetyears))
-
+  # get lon/lat information - not part of header yet
+  dimnames <- names(file_nc$dim)
+  latdim <- which(grepl("latitude", dimnames, ignore.case = TRUE))
+  londim <- which(grepl("longitude", dimnames, ignore.case = TRUE))
+  lon <- file_nc$dim[[londim]]$vals
+  lat <- file_nc$dim[[latdim]]$vals
+  nlonin <- length(lon)
+  nlatin <- length(lat)
+  
+  print(paste(nlonin, nlatin, nc_header$nbands, nc_header$nstep, ngetyears,sep = ","))
+  outdata <- array(0, dim = c(nlonin, nlatin, nc_header$nbands, nc_header$nstep, ngetyears))
 
   # get spatial extent and resolution
   # this will give a warning if the NetCDF has more than one data field, 
   # e.g. crop bands or time axis
   file_raster <- raster::raster(nc_in_file)
-  cells <- raster::cellFromXY(file_raster, cbind(lon,lat))
   for (year in get_year_start:get_year_stop){
-    for (month in 1:nmonths){
-      if (nbands == 1){
-        print(paste(year,firstyr,month,nmonths,nbands,((year - firstyr)*nmonths + month),sep=","))
+    for (step in 1:nc_header$nstep){
+      if (nc_header$nbands == 1){
+        print(paste(year,nc_header$firstyear,step,nc_header$nstep,nc_header$nbands,((year - nc_header$firstyear)*nc_header$nstep + step),sep=","))
         data <- ncdf4::ncvar_get(nc = file_nc, varid = var, count=c(-1,-1,1),
-                              start=c(1,1,((year - firstyr)*nmonths + month)))
+                              start=c(1,1,((year - nc_header$firstyear)*nc_header$nstep + step)))
         # check whether data needs to be flipped vertically
         # lat axes in nc files can be from north - south or from south - north
         if((raster::yFromRow(file_raster, 1) < raster::yFromRow(file_raster, 2)) 
@@ -223,11 +229,12 @@ read_cdf <- function( nc_in_file,
           # flip vertically
           data <- data[, nlatin:1]
         }
-        outdata[,,1,month,(year - get_year_start + 1)] <- data
+        outdata[,,1,step,(year - get_year_start + 1)] <- data
 
       }else{ #nbands>1
-        data <- ncvar_get(nc = file_nc, varid = var, count=c(-1,-1,-1,1),
-                          start=c(1,1,1,((year - firstyr)*nmonths + month)))
+        print(paste(year,nc_header$firstyear,step,nc_header$nstep,nc_header$nbands,((year - nc_header$firstyear)*nc_header$nstep + step),sep=","))
+        data <- ncdf4::ncvar_get(nc = file_nc, varid = var, count=c(-1,-1,-1,1),
+                          start=c(1,1,1,((year - nc_header$firstyear)*nc_header$nstep + step)))
         # check whether data needs to be flipped vertically
         # lat axes in nc files can be from north - south or from south - north
         if((raster::yFromRow(file_raster, 1) < raster::yFromRow(file_raster, 2)) 
@@ -235,16 +242,16 @@ read_cdf <- function( nc_in_file,
           # flip vertically
           data <- data[, nlatin:1,]
         }
-        outdata[,,,month,(year - get_year_start + 1)] <- data
+        outdata[,,,step,(year - get_year_start + 1)] <- data
 
       }# end if nbands == 1
-    }# end for month in 1:nmonths
+    }# end for step in 1:nc_header$nstep
   }# end for year in ...
   ncdf4::nc_close(file_nc)
-  dim(outdata) <- c(lon = nlonin, lat = nlatin, band = nbands,
-                    step = nmonths, year = ngetyears)
-  dimnames(outdata) <- list(lon = lon, lat = lat, band = 1:nbands,
-                            step = 1:nmonths, year = get_year_start:get_year_stop)
+  dim(outdata) <- c(lon = nlonin, lat = nlatin, band = nc_header$nbands,
+                    step = nc_header$nstep, year = ngetyears)
+  dimnames(outdata) <- list(lon = lon, lat = lat, band = 1:nc_header$nbands,
+                            step = 1:nc_header$nstep, year = get_year_start:get_year_stop)
 
   return(outdata)
 }
--- tests/test_cdf.R
+++ tests/test_cdf.R
@@ -1,8 +1,14 @@
+#devtools::load_all("/p/projects/open/Fabian/LPJbox/lpjmlkit_read_cdf/")
+devtools::load_all("/home/stenzel/lpjmlkit/")
+
 fpc_file <- "/p/projects/amazonas/nfan/output/LPJmL57_spitfire_WFDE5-CRU_wscal/output/fpc.nc"
 fpc_file_meta <- "/p/projects/amazonas/nfan/output/LPJmL57_spitfire_WFDE5-CRU_wscal/output/fpc.nc.json"
 
-lpjmlkit::detect_io_type(filename = fpc_file)
+head <- lpjmlkit::read_cdf_header(nc_in_file = fpc_file)
+data <- lpjmlkit::read_cdf(nc_in_file = fpc_file,
+                   nc_header = head
+                    )
 
-devtools::load_all("/p/projects/open/Fabian/LPJbox/lpjmlkit_read_cdf/")
-lpjmlkit::read_io(filename = fpc_file)
-
+mnpp_file <- "/p/projects/amazonas/nfan/output/LPJmL57_spitfire_WFDE5-CRU_wscal/output/mnpp.nc"
+monthly_head <- lpjmlkit::read_cdf_header(nc_in_file = mnpp_file) # takes veery long to create the LPJmLMeta object
+monthly_data <- lpjmlkit::read_cdf(nc_in_file = mnpp_file, nc_header = monthly_head)
